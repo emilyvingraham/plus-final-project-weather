13e13a5e59dd1adda7dced006c62a8749a21e369
Emily Ingraham <emilyvingraham@gmail.com>
2023-12-31 14:01:54 -0700

    Added and tested search engine functionality (no API integration yet)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+function searchForCity(event) {
+  event.preventDefault();
+  let cityName = document.querySelector('#city-name');
+  let searchInput = document.querySelector('#input-text');
+  cityName.innerHTML = searchInput.value;
+}
 
+let formElement = document.querySelector('#form');
+formElement.addEventListener('submit', searchForCity);
